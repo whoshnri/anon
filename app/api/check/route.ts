28b3034597b5076ecca1f48e5ec72dde102c8a60
Henry <henrybassey2007@gmail.com>
2025-07-06 00:32:15 +0100

    im tired asf

--- app/api/check/route.ts
+++ app/api/check/route.ts
@@ -5,9 +5,12 @@ export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const slug = searchParams.get('user') ?? '';
 
-
+  console.log(slug)
   const result = await db.query('SELECT * FROM users WHERE username = $1', [slug]);
     const user = result.rows[0];
+  console.log(user)
+
+
 
   return NextResponse.json({ valid: !!user });
 }
